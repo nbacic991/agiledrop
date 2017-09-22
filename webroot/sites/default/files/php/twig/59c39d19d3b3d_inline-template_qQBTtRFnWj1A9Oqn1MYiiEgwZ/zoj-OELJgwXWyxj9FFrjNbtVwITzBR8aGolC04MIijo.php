0897afaac1161724c7a9d29e1100ab9a8356d03f
Nemanja Bačić <nbacic@ymail.com>
2017-09-22 11:26:23 +0200

    Made pages look same as pages in provided assets.

---
+++ webroot/sites/default/files/php/twig/59c39d19d3b3d_inline-template_qQBTtRFnWj1A9Oqn1MYiiEgwZ/zoj-OELJgwXWyxj9FFrjNbtVwITzBR8aGolC04MIijo.php
@@ -0,0 +1,65 @@
+<?php
+
+/* {# inline_template_start #}{{ nothing }} */
+class __TwigTemplate_2ef038f0d59154e5bf39d129f4d20864ff5e27aaf1694dea6c5fd0a2b207d37d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["nothing"]) ? $context["nothing"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}{{ nothing }}";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{# inline_template_start #}{{ nothing }}";
+    }
+}
